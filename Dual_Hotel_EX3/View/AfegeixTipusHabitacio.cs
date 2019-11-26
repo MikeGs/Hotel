190424e467a48665d46730f39be855c3a74fffd8
Albert Gonzalez <56592643+Albert-cmd@users.noreply.github.com>
2019-11-26 21:01:09 +0100

    Vista Afegeix Tipus habitacio creada

---
+++ Dual_Hotel_EX3/View/AfegeixTipusHabitacio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dual_Hotel_EX3.View
+{
+    public partial class AfegeixTipusHabitacio : Form
+    {
+        public AfegeixTipusHabitacio()
+        {
+            InitializeComponent();
+        }
+
+        private void GestioHabitacio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
