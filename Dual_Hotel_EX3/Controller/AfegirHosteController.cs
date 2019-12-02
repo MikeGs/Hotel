aaab993be7b40a16a5c0fa51dfed209c5e97a53c
Albert Gonzalez <56592643+Albert-cmd@users.noreply.github.com>
2019-12-02 20:54:04 +0100

    ASD
    ASDSDASD

---
+++ Dual_Hotel_EX3/Controller/AfegirHosteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dual_Hotel_EX3.Controller
+{
+    class AfegirHosteController
+    {
+
+
+
+
+
+
+    }
+}
