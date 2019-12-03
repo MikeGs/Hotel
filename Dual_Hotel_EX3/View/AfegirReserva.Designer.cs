fa90a7e871fdfbd1af66155a58243444e9280132
Mike Gs <miquel18gs@gmail.com>
2019-12-03 19:56:55 +0100

    testing

--- Dual_Hotel_EX3/View/AfegirReserva.Designer.cs
+++ Dual_Hotel_EX3/View/AfegirReserva.Designer.cs
@@ -29,25 +29,29 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.SeleccionaHoste = new System.Windows.Forms.ComboBox();
-            this.DataInici = new System.Windows.Forms.TextBox();
-            this.DataFinal = new System.Windows.Forms.TextBox();
+            this.SeleccionaHosteInput = new System.Windows.Forms.ComboBox();
+            this.DataIniciInput = new System.Windows.Forms.TextBox();
+            this.DataFinalInput = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.Serveis = new System.Windows.Forms.DataGridView();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.importBaseInput = new System.Windows.Forms.TextBox();
+            this.IVAInput = new System.Windows.Forms.TextBox();
+            this.importTotalInput = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.ConfirmaReserva = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
-            this.SeleccionaPensio = new System.Windows.Forms.ComboBox();
-            this.label11 = new System.Windows.Forms.Label();
+            this.SeleccionaTipusInput = new System.Windows.Forms.ComboBox();
+            this.tipusHabitacioLabel = new System.Windows.Forms.Label();
+            this.temporadaLabel = new System.Windows.Forms.Label();
+            this.temporadaInput = new System.Windows.Forms.TextBox();
+            this.qtyLabel = new System.Windows.Forms.Label();
+            this.quantitatInput = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.Serveis)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,35 +66,35 @@
             this.label1.Text = "Afegeix una reserva";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // SeleccionaHoste
+            // SeleccionaHosteInput
             // 
-            this.SeleccionaHoste.FormattingEnabled = true;
-            this.SeleccionaHoste.Location = new System.Drawing.Point(60, 70);
-            this.SeleccionaHoste.Name = "SeleccionaHoste";
-            this.SeleccionaHoste.Size = new System.Drawing.Size(184, 21);
-            this.SeleccionaHoste.TabIndex = 1;
-            this.SeleccionaHoste.SelectedIndexChanged += new System.EventHandler(this.SeleccionaHoste_SelectedIndexChanged);
+            this.SeleccionaHosteInput.FormattingEnabled = true;
+            this.SeleccionaHosteInput.Location = new System.Drawing.Point(61, 56);
+            this.SeleccionaHosteInput.Name = "SeleccionaHosteInput";
+            this.SeleccionaHosteInput.Size = new System.Drawing.Size(184, 21);
+            this.SeleccionaHosteInput.TabIndex = 1;
+            this.SeleccionaHosteInput.SelectedIndexChanged += new System.EventHandler(this.SeleccionaHoste_SelectedIndexChanged);
             // 
-            // DataInici
+            // DataIniciInput
             // 
-            this.DataInici.Location = new System.Drawing.Point(60, 328);
-            this.DataInici.Name = "DataInici";
-            this.DataInici.Size = new System.Drawing.Size(184, 20);
-            this.DataInici.TabIndex = 3;
-            this.DataInici.TextChanged += new System.EventHandler(this.DataInici_TextChanged);
+            this.DataIniciInput.Location = new System.Drawing.Point(61, 314);
+            this.DataIniciInput.Name = "DataIniciInput";
+            this.DataIniciInput.Size = new System.Drawing.Size(184, 20);
+            this.DataIniciInput.TabIndex = 3;
+            this.DataIniciInput.TextChanged += new System.EventHandler(this.DataInici_TextChanged);
             // 
-            // DataFinal
+            // DataFinalInput
             // 
-            this.DataFinal.Location = new System.Drawing.Point(60, 374);
-            this.DataFinal.Name = "DataFinal";
-            this.DataFinal.Size = new System.Drawing.Size(184, 20);
-            this.DataFinal.TabIndex = 4;
-            this.DataFinal.TextChanged += new System.EventHandler(this.DataFinal_TextChanged);
+            this.DataFinalInput.Location = new System.Drawing.Point(61, 360);
+            this.DataFinalInput.Name = "DataFinalInput";
+            this.DataFinalInput.Size = new System.Drawing.Size(184, 20);
+            this.DataFinalInput.TabIndex = 4;
+            this.DataFinalInput.TextChanged += new System.EventHandler(this.DataFinal_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(60, 51);
+            this.label2.Location = new System.Drawing.Point(61, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 5;
@@ -99,7 +103,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(60, 149);
+            this.label3.Location = new System.Drawing.Point(61, 135);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 13);
             this.label3.TabIndex = 6;
@@ -108,7 +112,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(60, 312);
+            this.label4.Location = new System.Drawing.Point(61, 298);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(52, 13);
             this.label4.TabIndex = 7;
@@ -117,7 +121,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(60, 358);
+            this.label5.Location = new System.Drawing.Point(61, 344);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(55, 13);
             this.label5.TabIndex = 8;
@@ -127,7 +131,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(280, 51);
+            this.label6.Location = new System.Drawing.Point(281, 37);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(100, 16);
             this.label6.TabIndex = 9;
@@ -136,42 +140,42 @@
             // Serveis
             // 
             this.Serveis.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Serveis.Location = new System.Drawing.Point(60, 165);
+            this.Serveis.Location = new System.Drawing.Point(61, 151);
             this.Serveis.Name = "Serveis";
             this.Serveis.Size = new System.Drawing.Size(184, 143);
             this.Serveis.TabIndex = 10;
             // 
-            // textBox3
+            // importBaseInput
             // 
-            this.textBox3.Location = new System.Drawing.Point(280, 124);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(110, 20);
-            this.textBox3.TabIndex = 11;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.importBaseInput.Location = new System.Drawing.Point(281, 110);
+            this.importBaseInput.Name = "importBaseInput";
+            this.importBaseInput.ReadOnly = true;
+            this.importBaseInput.Size = new System.Drawing.Size(110, 20);
+            this.importBaseInput.TabIndex = 11;
+            this.importBaseInput.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox1
+            // IVAInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(280, 186);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(110, 20);
-            this.textBox1.TabIndex = 12;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.IVAInput.Location = new System.Drawing.Point(281, 172);
+            this.IVAInput.Name = "IVAInput";
+            this.IVAInput.ReadOnly = true;
+            this.IVAInput.Size = new System.Drawing.Size(110, 20);
+            this.IVAInput.TabIndex = 12;
+            this.IVAInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // importTotalInput
             // 
-            this.textBox2.Location = new System.Drawing.Point(280, 248);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(110, 20);
-            this.textBox2.TabIndex = 13;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.importTotalInput.Location = new System.Drawing.Point(281, 234);
+            this.importTotalInput.Name = "importTotalInput";
+            this.importTotalInput.ReadOnly = true;
+            this.importTotalInput.Size = new System.Drawing.Size(110, 20);
+            this.importTotalInput.TabIndex = 13;
+            this.importTotalInput.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(280, 105);
+            this.label7.Location = new System.Drawing.Point(281, 91);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(66, 13);
             this.label7.TabIndex = 14;
@@ -180,7 +184,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(277, 170);
+            this.label8.Location = new System.Drawing.Point(278, 156);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(24, 13);
             this.label8.TabIndex = 15;
@@ -189,7 +193,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(277, 232);
+            this.label9.Location = new System.Drawing.Point(278, 218);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(63, 13);
             this.label9.TabIndex = 16;
@@ -198,7 +202,7 @@
             // 
             // ConfirmaReserva
             // 
-            this.ConfirmaReserva.Location = new System.Drawing.Point(283, 372);
+            this.ConfirmaReserva.Location = new System.Drawing.Point(375, 387);
             this.ConfirmaReserva.Name = "ConfirmaReserva";
             this.ConfirmaReserva.Size = new System.Drawing.Size(163, 22);
             this.ConfirmaReserva.TabIndex = 17;
@@ -210,54 +214,91 @@
             // 
             this.label10.AutoSize = true;
             this.label10.ForeColor = System.Drawing.Color.Red;
-            this.label10.Location = new System.Drawing.Point(283, 344);
+            this.label10.Location = new System.Drawing.Point(278, 331);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(127, 13);
             this.label10.TabIndex = 18;
             this.label10.Text = "ERROR EN LES DADES";
             // 
-            // SeleccionaPensio
+            // SeleccionaTipusInput
             // 
-            this.SeleccionaPensio.FormattingEnabled = true;
-            this.SeleccionaPensio.Location = new System.Drawing.Point(60, 121);
-            this.SeleccionaPensio.Name = "SeleccionaPensio";
-            this.SeleccionaPensio.Size = new System.Drawing.Size(184, 21);
-            this.SeleccionaPensio.TabIndex = 19;
+            this.SeleccionaTipusInput.FormattingEnabled = true;
+            this.SeleccionaTipusInput.Location = new System.Drawing.Point(61, 107);
+            this.SeleccionaTipusInput.Name = "SeleccionaTipusInput";
+            this.SeleccionaTipusInput.Size = new System.Drawing.Size(133, 21);
+            this.SeleccionaTipusInput.TabIndex = 19;
             // 
-            // label11
+            // tipusHabitacioLabel
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(57, 105);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(82, 13);
-            this.label11.TabIndex = 20;
-            this.label11.Text = "Tipus de pensio";
-            this.label11.Click += new System.EventHandler(this.label11_Click);
+            this.tipusHabitacioLabel.AutoSize = true;
+            this.tipusHabitacioLabel.Location = new System.Drawing.Point(58, 91);
+            this.tipusHabitacioLabel.Name = "tipusHabitacioLabel";
+            this.tipusHabitacioLabel.Size = new System.Drawing.Size(87, 13);
+            this.tipusHabitacioLabel.TabIndex = 20;
+            this.tipusHabitacioLabel.Text = "Tipus d\'habitació";
+            this.tipusHabitacioLabel.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // temporadaLabel
+            // 
+            this.temporadaLabel.AutoSize = true;
+            this.temporadaLabel.Location = new System.Drawing.Point(278, 275);
+            this.temporadaLabel.Name = "temporadaLabel";
+            this.temporadaLabel.Size = new System.Drawing.Size(61, 13);
+            this.temporadaLabel.TabIndex = 22;
+            this.temporadaLabel.Text = "Temporada";
+            // 
+            // temporadaInput
+            // 
+            this.temporadaInput.Location = new System.Drawing.Point(281, 291);
+            this.temporadaInput.Name = "temporadaInput";
+            this.temporadaInput.ReadOnly = true;
+            this.temporadaInput.Size = new System.Drawing.Size(110, 20);
+            this.temporadaInput.TabIndex = 21;
+            // 
+            // qtyLabel
+            // 
+            this.qtyLabel.AutoSize = true;
+            this.qtyLabel.Location = new System.Drawing.Point(197, 91);
+            this.qtyLabel.Name = "qtyLabel";
+            this.qtyLabel.Size = new System.Drawing.Size(50, 13);
+            this.qtyLabel.TabIndex = 23;
+            this.qtyLabel.Text = "Quantitat";
+            // 
+            // quantitatInput
+            // 
+            this.quantitatInput.Location = new System.Drawing.Point(200, 108);
+            this.quantitatInput.Name = "quantitatInput";
+            this.quantitatInput.Size = new System.Drawing.Size(45, 20);
+            this.quantitatInput.TabIndex = 24;
             // 
             // AfegirReserva
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(550, 421);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.SeleccionaPensio);
+            this.Controls.Add(this.quantitatInput);
+            this.Controls.Add(this.qtyLabel);
+            this.Controls.Add(this.temporadaLabel);
+            this.Controls.Add(this.temporadaInput);
+            this.Controls.Add(this.tipusHabitacioLabel);
+            this.Controls.Add(this.SeleccionaTipusInput);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.ConfirmaReserva);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.importTotalInput);
+            this.Controls.Add(this.IVAInput);
+            this.Controls.Add(this.importBaseInput);
             this.Controls.Add(this.Serveis);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.DataFinal);
-            this.Controls.Add(this.DataInici);
-            this.Controls.Add(this.SeleccionaHoste);
+            this.Controls.Add(this.DataFinalInput);
+            this.Controls.Add(this.DataIniciInput);
+            this.Controls.Add(this.SeleccionaHosteInput);
             this.Controls.Add(this.label1);
             this.Name = "AfegirReserva";
             this.Text = "AfegirReserva";
@@ -271,24 +312,28 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox SeleccionaHoste;
-        private System.Windows.Forms.TextBox DataInici;
-        private System.Windows.Forms.TextBox DataFinal;
+        private System.Windows.Forms.ComboBox SeleccionaHosteInput;
+        private System.Windows.Forms.TextBox DataIniciInput;
+        private System.Windows.Forms.TextBox DataFinalInput;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.DataGridView Serveis;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox importBaseInput;
+        private System.Windows.Forms.TextBox IVAInput;
+        private System.Windows.Forms.TextBox importTotalInput;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button ConfirmaReserva;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.ComboBox SeleccionaPensio;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox SeleccionaTipusInput;
+        private System.Windows.Forms.Label tipusHabitacioLabel;
+        private System.Windows.Forms.Label temporadaLabel;
+        private System.Windows.Forms.TextBox temporadaInput;
+        private System.Windows.Forms.Label qtyLabel;
+        private System.Windows.Forms.TextBox quantitatInput;
     }
 }
